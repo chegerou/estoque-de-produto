3e91d47ed1cebb4b96bb56165d893774461bc8b4
Igor Afonso Nunes <igorafonsonunes@outlook.com.br>
2020-01-04 19:39:55 -0300

    Atualizando script

--- Scripts de Criação/CriacaoDaTabelaProduto.sql
+++ Scripts de Criação/CriacaoDaTabelaProduto.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Produto(
-Id int IDENTITY(100,1) primary key,
-Nome varchar (200),
-Quantidade int,
-Preco int
+Id INT IDENTITY(100,1) PRIMARY KEY,
+Nome VARCHAR (200),
+Quantidade INT,
+Preco FLOAT
 )
   
