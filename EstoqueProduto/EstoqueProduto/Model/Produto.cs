72acb5e590d21ad100ec703a91805fd5e8800211
Igor Afonso Nunes <igorafonsonunes@outlook.com.br>
2020-01-04 15:59:23 -0300

    Criado classe Produto no serviço de Estoque Produto

---
+++ EstoqueProduto/EstoqueProduto/Model/Produto.cs
@@ -0,0 +1,25 @@
+﻿using Data;
+
+namespace EstoqueProduto.Model
+{
+    public class Produto
+    {
+
+        public string getProdutos()
+        {
+            using (var context = new DataContext())
+            {
+                string objetoProduto = context
+
+
+
+            }
+        }
+
+
+
+
+
+
+    }
+}
