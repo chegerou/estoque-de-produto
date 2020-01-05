8d852eb33fde7eb27863b6da63f73d0e6db539de
Igor Afonso Nunes <igorafonsonunes@outlook.com.br>
2020-01-05 00:17:06 -0300

    Criação do projeto angular

---
+++ EstoqueProduto - Front/estoque-produto/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolBarComponent } from './src/tool-bar/tool-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
